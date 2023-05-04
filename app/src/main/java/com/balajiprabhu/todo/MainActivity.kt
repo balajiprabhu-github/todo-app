3c43389916de8eaf9955b369b4adf2fc4ce48d56
balajiprabhu <41998731+balajiprabhu-github@users.noreply.github.com>
2023-05-04 10:02:48 +0530

    Initial commit

---
+++ app/src/main/java/com/balajiprabhu/todo/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.balajiprabhu.todo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.balajiprabhu.todo.ui.theme.TodoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TodoTheme {
+
+            }
+        }
+    }
+}
